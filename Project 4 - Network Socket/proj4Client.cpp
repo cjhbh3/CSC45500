1a38e3dac33221c231fb92c78048902861523e28
CJ Hess <cjh147@lindenwood.edu>
2023-04-18 10:24:38 -0500

    Added example code

--- Project 4 - Network Socket/proj4Client.cpp
+++ Project 4 - Network Socket/proj4Client.cpp
@@ -4,11 +4,84 @@
     File: proj4Client.cpp
 */
 
+#include <netdb.h>
+#include <unistd.h>
+
+#include <cstdlib>
 #include <iostream>
+
+#define SUCCESS 0
+#define USAGE_ERROR 1
+#define HOST_ERROR 2
+#define SOCKET_ERROR 3
+#define CONNECT_ERROR 4
+#define READ_ERROR 5
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
+int do_client(char *serverName);
 
-    return 0;
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        cout << "Error: Usage: " << argv[0] << " <TOD-server>" << endl;
+        return USAGE_ERROR;
+    }
+    return do_client(argv[1]);
+}
+
+int do_client(char *serverName)
+{
+  struct hostent *serverEntry;
+  serverEntry = gethostbyname(serverName);
+  if (!serverEntry)
+    {
+      cerr << "gethostbyname() failed on: " << serverName << endl;
+      return HOST_ERROR;
+    }
+
+  struct sockaddr_in serverInfo;
+  serverInfo.sin_family = AF_INET; // IPV4
+  serverInfo.sin_addr = * (struct in_addr *) serverEntry->h_addr_list[0];
+  serverInfo.sin_port = htons(13); 
+  
+  int mySocket = socket(AF_INET, SOCK_STREAM, 0);
+  if (mySocket == 0) // socket() call failed
+    {
+      cerr << "socket() call for creating socket failed!" << endl;
+      return SOCKET_ERROR;
+    }
+
+  int rVal =
+    connect(mySocket, (struct sockaddr *) &serverInfo, sizeof(serverInfo));
+
+  if (rVal<0)
+    {
+      cerr << "connect() failed!" << endl;
+      return CONNECT_ERROR; 
+    }
+
+  // now, we're ready to "talk with" the server!
+  char buffer[81];
+  int charsRead;
+
+  charsRead = read(mySocket, buffer, 80);
+
+  while (charsRead>0)
+    {
+      buffer[charsRead] = '\0'; // terminate string sentfrom server.
+      cout << buffer;
+
+      charsRead = read(mySocket, buffer, 80);
+    }
+
+  if (charsRead <0)
+    {
+      cerr << "read() failed. " << endl;
+      return READ_ERROR;
+    }
+
+
+  close(mySocket); // let server know we are done. 
+  
+  return SUCCESS;
 }
--- Project 4 - Network Socket/proj4Server.cpp
+++ Project 4 - Network Socket/proj4Server.cpp
@@ -4,11 +4,120 @@
     File: proj4Server.cpp
 */
 
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#include <pthread.h>
+
 #include <iostream>
+#include <cstdlib>
+#include <string>
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
+struct mywork_struct {
+    int with_sock;
+    struct sockaddr_in *from_cli;
+};
 
-    return 0;
+#define USAGE_ERROR 1
+#define SOCK_ERROR 2
+#define BIND_ERROR 3
+#define LISTEN_ERROR 4
+
+#define MAX_WAITERS 25
+
+int do_server(int portNum);
+void* do_work(void *pthreadParam);
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        cerr << "Usage: " << argv[0] << " <port-number>" << endl;
+        return USAGE_ERROR;
+    }
+
+    return do_server(atoi(argv[1]));
+}
+
+int do_server(int portNum) {
+    int listen_sock;
+
+    struct sockaddr_in local_addr;
+
+    listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (listen_sock < 0) {
+        cerr << "Could not create listening socket" << endl;
+        return SOCK_ERROR;
+    }
+
+    local_addr.sin_family = AF_INET;
+    local_addr.sin_addr.s_addr = INADDR_ANY;
+    local_addr.sin_port = htons(portNum);
+
+    int status = bind(listen_sock, (sockaddr*) &local_addr, sizeof(local_addr));
+    if (status!=0) {
+        cerr << "bind() error" << endl;
+        return BIND_ERROR;
+    }
+
+    status = listen(listen_sock, MAX_WAITERS);
+    if (status!=0) {
+        cerr << "listen() error" << endl;
+        return LISTEN_ERROR;
+    }
+    
+    while( true ) {
+      int connected_sock;        // socket to build for incoming client
+      struct sockaddr_in from;   // holds client address data
+      unsigned int from_len;     // holds size of client address
+      
+      from_len = sizeof(from);
+      
+      connected_sock = accept(listen_sock, (sockaddr*) &from, &from_len);
+      // do_work(connected_sock, &from); 
+      struct mywork_struct *params = new mywork_struct;
+      params -> with_sock = connected_sock;
+      params -> from_cli = &from;
+      
+      pthread_create(new pthread_t, NULL, do_work, (void *) params);
+    }
+}
+
+//void do_work(int conn_sock, struct sockaddr_in *client_addr)
+void* do_work(void *param) {
+  struct mywork_struct *mwsp = (mywork_struct *) param;
+  int conn_sock = mwsp -> with_sock;
+  struct sockaddr_in *client_addr = mwsp -> from_cli;
+
+  // suggested idea: delete now unused structure:
+  delete mwsp;
+  
+  string buffer;
+
+  // build the response string to send to the client
+  buffer =  "Your IP address is: ";
+  buffer += inet_ntoa(client_addr->sin_addr);
+  buffer += '\n';
+
+  char *cbuff = (char *) buffer.c_str(); 
+
+  int needed = buffer.length();
+
+  // need to put in a loop ...
+  while(needed>0)
+    {
+      int num_written= write(conn_sock, cbuff, needed);
+
+      needed -= num_written;
+      cbuff += num_written;
+    }
+
+  
+  cout << "LOG: processed a connection from "
+       <<  inet_ntoa(client_addr->sin_addr) << endl;
+
+  close(conn_sock);  // closed connected socket - we are done with this client!
+
+  return nullptr;
 }
